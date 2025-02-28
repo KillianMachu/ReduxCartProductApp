79fe48757e13638f52494ed1d33d59bf453cb752
Killian <killian_machu@ens.univ-artois.fr>
2025-02-28 02:07:52 +0100

    Product page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useSelector } from "react-redux";
+import { RootState } from "./store/store";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import { HomeIcon, HeartIcon, ShoppingCartIcon } from "@heroicons/react/24/outline";
@@ -7,6 +9,11 @@ import ProductList from "./components/ProductList";
 import Wishlist from "./components/Wishlist";
 
 const App = () => {
+  const cartItems = useSelector((state: RootState) => state.cart.items);
+  let totalItems = 0;
+  cartItems.forEach((item) => {
+    totalItems += item.quantity ?? 0;
+  });
   return (
     <Router>
       <div className="min-w-screen min-h-screen bg-[url(/public/background3.jpg)] bg-cover backdrop-blur-3xl bg-fixed font-content">
@@ -14,7 +21,7 @@ const App = () => {
           <nav className="flex justify-center bg-white rounded-full bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-40 border border-gray-100 mx-auto w-fit px-4 text-white font-semibold gap-2">
             <Link className="m-1 rounded-full hover:bg-gray-700 hover:bg-opacity-30 p-2 transition duration-300" to="/"><HomeIcon className="h-8 w-8" /></Link>
             <Link className="m-1 rounded-full hover:bg-gray-700 hover:bg-opacity-30 p-2 transition duration-300" to="/wishlist"><HeartIcon className="h-8 w-8" /></Link>
-            <Link className="m-1 rounded-full hover:bg-gray-700 hover:bg-opacity-30 p-2 transition duration-300" to="/cart"><ShoppingCartIcon className="h-8 w-8" /></Link>
+            <Link className="m-1 rounded-full hover:bg-gray-700 hover:bg-opacity-30 p-2 transition duration-300 relative" to="/cart"><ShoppingCartIcon className="h-8 w-8" />{totalItems === 0 ? '' : <span className="absolute top-2 right-2 flex h-4 w-4 bg-gray-600 text-white text-sm rounded-full justify-center items-center">{totalItems}</span>}</Link>
           </nav>
           <Routes>
             <Route path="/" element={<ProductList />} />
--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -1,7 +1,10 @@
 import { useSelector, useDispatch } from "react-redux";
 import { AppDispatch, RootState } from "../store/store";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import { addToCart } from "../store/cartSlice";
+import { Squares2X2Icon, TagIcon, ChevronDownIcon, ChevronUpIcon } from "@heroicons/react/24/outline";
+import StarRating from "./StarRating";
 
 const ProductDetails = () => {
   const dispatch = useDispatch() as AppDispatch;
@@ -13,8 +16,10 @@ const ProductDetails = () => {
 
   if (!product) return <p className="text-center text-gray-500 mt-8">Produit non trouvé</p>;
 
+  const [isVisibleInfos, setIsVisibleInfos] = React.useState(false);
+
   return (
-    <div className="max-w-5xl mx-auto px-4 py-8">
+    <div className="max-w-6xl mx-auto px-4 py-8">
       {/* Header Section */}
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center ">
         <img
@@ -22,43 +27,56 @@ const ProductDetails = () => {
           alt={product.title}
           className="w-full h-auto object-cover rounded-lg shadow-lg bg-gray-300"
         />
-        <div>
-          <h1 className="text-4xl font-bold text-white mb-4">{product.title}</h1>
-          <p className="text-gray-100 text-lg mb-2">Catégorie : {product.category}</p>
-          <p className="text-gray-100 text-lg">Marque : {product.brand}</p>
+        <div className="h-full flex flex-col justify-around">
+          <div>
+            <h1 className="text-4xl font-bold text-white mb-4">{product.title}</h1>
+            <div className="flex gap-4 mb-4">
+              <p className="flex px-6 py-2 w-fit items-center gap-2 rounded-full bg-white bg-opacity-20 text-white"><Squares2X2Icon className="w-6 h-6 text-white" /> {product.category.charAt(0).toUpperCase() + product.category.slice(1)}</p>
+              <p className="flex px-6 py-2 w-fit items-center gap-2 rounded-full bg-white bg-opacity-20 text-white"><TagIcon className="w-6 h-6 text-white" /> {product.brand}</p>
+            </div>
+            <div className="flex justify-between">
+              <p className="text-sm flex gap-4 text-gray-300 mb-4"><StarRating rating={product.rating} /> {product.rating}/5</p>
+              <p className="text-gray-300 flex">Stock restant : {product.stock} (<span className="text-green-300 font-medium mb-4">{product.availabilityStatus}</span>)</p>
+            </div>
+            <p className="text-gray-100 text-lg mb-6">{product.description}</p>
+          </div>
+          <div className="flex justify-between text-white items-center">
+            <p className="text-4xl font-semibold mb-4">
+              {product.price} € <span className="text-sm text-gray-300">(-{product.discountPercentage}%)</span>
+            </p>
+            <button
+              onClick={() => dispatch(addToCart(product))}
+              className="m-1 w-fit rounded-full bg-white hover:bg-opacity-20 hover:bg-gray-700 text-gray-600 hover:text-white border border-white px-4 py-2 transition duration-100 text-xl"
+            >
+              Ajouter au panier
+            </button>
+          </div>
         </div>
-        <button
-          onClick={() => dispatch(addToCart(product))}
-          className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 w-full"
-        >
-          Ajouter au panier
-        </button>
       </div>
 
       {/* Main Content */}
-      <div className="mt-10 grid grid-cols-1 md:grid-cols-2 gap-8">
-        {/* Product Info */}
-        <div>
-          <p className="text-white text-lg mb-6">{product.description}</p>
-          <p className="text-2xl font-semibold text-red-600 mb-4">
-            Prix : {product.price} EUR <span className="text-sm text-gray-500">(-{product.discountPercentage}%)</span>
-          </p>
-          <p className="text-green-600 font-medium mb-4">{product.availabilityStatus}</p>
-          <p className="text-gray-100 text-lg">Évaluation : {product.rating} / 5</p>
-          <p className="text-gray-100 text-lg">Stock restant : {product.stock}</p>
+      {/* Metadata */}
+      <div className="mt-10 bg-white bg-opacity-20 placeholder-white border-y border-gray-300 text-white p-6">
+        <div className="flex justify-between items-center">
+          <h3 className="text-xl font-bold text-white">Informations</h3>
+          <button
+            onClick={() => setIsVisibleInfos(!isVisibleInfos)}
+            className="text-white">
+            {isVisibleInfos ? <ChevronUpIcon className="w-8 h-8 text-white" strokeWidth={3} /> : <ChevronDownIcon className="w-8 h-8 text-white" strokeWidth={3} />}
+            </button>
         </div>
-
-        {/* Metadata */}
-        <div className="bg-white bg-opacity-20 placeholder-white border border-gray-300 text-white p-6 rounded-lg">
-          <p className="mb-2">SKU : {product.sku}</p>
-          <p className="mb-2">Poids : {product.weight} kg</p>
-          <p className="mb-2">
-            Dimensions : {product.dimensions.width} x {product.dimensions.height} x {product.dimensions.depth} cm
-          </p>
-          <p className="mb-2">Garantie : {product.warrantyInformation}</p>
-          <p className="mb-2">Politique de retour : {product.returnPolicy}</p>
-          <p className="mb-2">Livraison : {product.shippingInformation}</p>
-        </div>
+        {isVisibleInfos && (
+          <div className="mt-5">
+            <p className="mb-2">SKU : {product.sku}</p>
+            <p className="mb-2">Poids : {product.weight} kg</p>
+            <p className="mb-2">
+              Dimensions : {product.dimensions.width} x {product.dimensions.height} x {product.dimensions.depth} cm
+            </p>
+            <p className="mb-2">Garantie : {product.warrantyInformation}</p>
+            <p className="mb-2">Politique de retour : {product.returnPolicy}</p>
+            <p className="mb-2">Livraison : {product.shippingInformation}</p>
+          </div>
+        )}
       </div>
 
       {/* Reviews */}
@@ -82,7 +100,7 @@ const ProductDetails = () => {
           ))}
         </div>
       </div>
-    </div>
+    </div >
   );
 };
 
